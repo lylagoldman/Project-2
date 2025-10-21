16eee537da5efb05fc7b9700cb0264537117968d
Lyla Goldman <lylagoldman@gmail.com>
2025-10-21 00:01:11 -0400

    Initial Commit

--- Project 2 - Weather App/ContentView.swift
+++ Project 2 - Weather App/ContentView.swift
@@ -7,13 +7,71 @@
 
 import SwiftUI
 
+struct Hour {
+    let id = UUID()
+    var time: String
+}
+
+struct Day {
+    let id = UUID()
+    var name: String
+}
+struct HourlyRowView: View {
+    let times = ["Now", "1PM", "2PM", "3PM", "4PM", "5PM"]
+    let symbols = ["sun.max.fill", "cloud.sun.fill", "cloud.sun.rain.fill"]
+    var body: some View {
+        HStack {
+            ForEach(times, id: \.self) { time in
+                Text(time)
+                    .fontWeight(.semibold)
+                    .padding(.trailing, 19)
+            }
+            
+        }
+        
+        HStack {
+            Image(systemName: symbols[0])
+                .symbolRenderingMode(.multicolor)
+                .imageScale(.large)
+                .font(.title)
+            Image(systemName: symbols[0])
+                .symbolRenderingMode(.multicolor)
+                .imageScale(.large)
+                .font(.title)
+            Image(systemName: symbols[0])
+                .symbolRenderingMode(.multicolor)
+                .imageScale(.large)
+                .font(.title)
+            Image(systemName: symbols[1])
+                .symbolRenderingMode(.multicolor)
+                .imageScale(.large)
+                .font(.title)
+        }
+        
+    }
+}
+
+struct HourlyForecastView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "clock")
+                    .fontWeight(.semibold)
+                Text("HOURLY FORECAST")
+                    .fontWeight(.semibold)
+                Spacer()
+            }
+            .padding(.bottom, 5)
+            HourlyRowView()
+        }
+            
+    }
+}
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HourlyForecastView()
+            
         }
         .padding()
     }
